40d41a2e40e0d6afc46d848dffbffcfc20afc7d7
Garrett Anderson <keaton85@runbox.com>
2021-06-23 13:26:52 -0500

    create vars to access elements

---
+++ script.js
@@ -0,0 +1,15 @@
+var btnVal;
+var display = document.getElementById("display");
+var clear = document.getElementById("clear");
+var equals = document.getElementById("equals");
+var formula = document.getElementById("formula");
+var formulaString;
+
+var operators = document.querySelectorAll(".btn.operator");
+var decimal = document.querySelector("#decimal");
+var numbers = document.querySelectorAll(".btn.number");
+var multiply = document.querySelector("#multiply");
+var add = document.querySelector("#add");
+var subtract = document.querySelector("#subtract");
+var divide = document.querySelector("#divide");
+var plus_minus = document.getElementById("plus_minus");
