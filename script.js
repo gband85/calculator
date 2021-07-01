f98d34f8e128903314c54edfc45467741453e7ee
Garrett Anderson <keaton85@runbox.com>
2021-07-01 17:45:38 -0500

    remove new algo, add to branch

--- script.js
+++ script.js
@@ -178,6 +178,3 @@ const calc = {
     clearDisplay();
     updateDisplay();
   };
-
-  //set operatorEntered to operator, not true false
-  //if two operands + operator exist, perform calculation on next operator press
